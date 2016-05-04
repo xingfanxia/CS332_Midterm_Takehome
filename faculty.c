222a39a9f33b21b730c8c4bc748ec09bae29b576
xxf1995 <xiax@carleton.edu>
2016-05-04 16:42:16 -0500

    fuck it Fixed!!

--- faculty.c
+++ faculty.c
@@ -10,21 +10,20 @@ int numCS_lounge = 0; //number of CS prof in
 int numMath_lounge = 0; //number of Math prof
 int left = 0;
 int enters = 0;
+int left1 = 0;
+int enters1 = 0;
+
 int sign = 0; //1 for Math, 2 for CS
-int totalnumber;
+
 
 pthread_mutex_t avail_lock;
-pthread_mutex_t leave_lock;
 pthread_cond_t CSwait;
 pthread_cond_t Mathwait;
-pthread_cond_t CSgo;
-pthread_cond_t Mathgo;
 
-
-sem_t* CS_Here;
-sem_t* Math_Here;
-sem_t* startThread_sem; //sem to start all the threads
-sem_t* waitThread_sem; //sem to check if all thread are created
+// sem_t* CS_Here;
+// sem_t* Math_Here;
+// sem_t* startThread_sem; //sem to start all the threads
+// sem_t* waitThread_sem; //sem to check if all thread are created
 
 void* mathProfArrive(void*);
 void* csProfArrive(void*);
@@ -34,7 +33,6 @@ void* Math(void*);
 void* CS(void*);
 void initSync();
 void closeSync();
-void delay(int);
 void shuffle(int*, int);
 
 
@@ -162,32 +160,14 @@ void* mathProfArrive(void* args) {
 		pthread_cond_wait(&Mathwait, &avail_lock);
 	}
 	printf("Math Prof enters lounge\n");
-	enters++;
 	fflush(stdout);
-	
+	enters++;
 	numMath_lounge++;
 	sign = 1;
 	// pthread_cond_signal(&Mathgo);
 	pthread_mutex_unlock(&avail_lock);
 	}
 
-	// pthread_mutex_lock(&avail_lock);
-	// // while (sign == 2) {
-	// // 	pthread_cond_wait(&Mathwait, &avail_lock);
-	// // 	printf("%d\n", sign);
-	// // 	printf("Math waiting\n");
-	// // }
-
-	// if (sign != 2) {//Math Prof got into the lounge here
-	// 	printf("Math Prof enters Lounge\n");
-	// 	sign = 1;
-	// 	fflush(stdout);
-	// 	//pthread_cond_signal(&Mathwait);
-	// 	numMath_out--;
-	// 	numMath_lounge++;
-		
-	// 	pthread_mutex_unlock(&avail_lock);
-	// }
 
 void* csProfArrive(void* args) {
 	printf("CS Prof arrives\n"); 
@@ -198,33 +178,12 @@ void* csProfArrive(void* args) {
 	}
 	printf("CS Prof enters lounge\n");
 	fflush(stdout);
+	enters1++;
 	numCS_lounge++;
 	sign = 2;
 	// pthread_cond_signal(&CSgo);
 	pthread_mutex_unlock(&avail_lock);
 	}
-
-	// sem_post(waitThread_sem); //tell main it is created
-	// sem_wait(startThread_sem); //wait for main to tell it to start
-	// pthread_mutex_lock(&avail_lock);
-	// // while (sign == 1) {
-	// // 	pthread_cond_wait(&CSwait, &avail_lock);
-	// // 	printf("%d\n", sign);
-	// // 	printf("CS waiting\n");
-	// // }
-
-	// if (sign != 1) {//CS Prof got into the lounge here
-	// 	printf("CS Prof enters Lounge\n");
-	// 	sign = 2;
-	// 	fflush(stdout);
-	// 	//pthread_cond_signal(&CSwait);
-	// 	numCS_out--;
-	// 	numCS_lounge++;
-	// 	// sem_post(&CSHere)
-	// 	pthread_mutex_unlock(&avail_lock);
-
-	// }
-
 
 void* mathProfLeave(void* args) {
 		
@@ -235,15 +194,11 @@ void* mathProfLeave(void* args) {
 		left++;
 		if (numMath_lounge == 0) {
 			sign = 0;
-			pthread_cond_signal(&CSwait);
+			pthread_cond_broadcast(&CSwait);
 		}
 		pthread_mutex_unlock(&avail_lock);
 		printf("Math prof enter/left: %d / %d\n", enters, left);
 		fflush(stdout);
-	// totalnumberleaving++;
-	// printf("#ofleft: %d\n", totalnumberleaving);
-	// int numtotal = numMath_out + numCS_out;
-	// printf("number total waiting is%d\n", numtotal);
 }
 
 void* csProfLeave(void* args) {
@@ -251,39 +206,32 @@ void* csProfLeave(void* args) {
 		printf("CS Prof leaves Lounge\n");
 		fflush(stdout);
 		numCS_lounge--;
+		left1++;
 		if (numCS_lounge == 0) {
 			sign = 0;
-			pthread_cond_signal(&Mathwait);
+			pthread_cond_broadcast(&Mathwait);
 		}
 		pthread_mutex_unlock(&avail_lock);
+		printf("CS prof enter/left: %d / %d\n", enters1, left1);
+		fflush(stdout);
 		
-
-
-	// totalnumberleaving++;
-	// printf("#ofleft: %d\n", totalnumberleaving);
-	// int numtotal = numMath_out + numCS_out;
-	// printf("number total waiting is%d\n", numtotal);
 }
 
 void initSync() { //init all sync stuff
 	pthread_mutex_init(&avail_lock, NULL);
 	pthread_cond_init(&CSwait, NULL);
 	pthread_cond_init(&Mathwait, NULL);
-	pthread_cond_init(&CSgo, NULL);
-	pthread_cond_init(&Mathgo, NULL);
-	
-	// pthread_cond_init(&cWaitAt1, NULL);
-	// pthread_cond_init(&cWaitToBow0, NULL);
-	// pthread_cond_init(&stopRowingback, NULL);
+	// pthread_cond_init(&CSgo, NULL);
+	// pthread_cond_init(&Mathgo, NULL);
 
 
 	// CS_Here = sem_open("CSHere", O_CREAT|O_EXCL, 0466, 0);
 
 	// Math_Here = sem_open("MathHere", O_CREAT|O_EXCL, 0466, 0);
 
-	startThread_sem = sem_open("startSem", O_CREAT|O_EXCL, 0466, 0);
+	// startThread_sem = sem_open("startSem", O_CREAT|O_EXCL, 0466, 0);
 
-	waitThread_sem = sem_open("waitSem", O_CREAT|O_EXCL, 0466, 0);
+	// waitThread_sem = sem_open("waitSem", O_CREAT|O_EXCL, 0466, 0);
 
 	// while (CS_Here==SEM_FAILED) {
 	//     if (errno == EEXIST) {
@@ -313,44 +261,41 @@ void initSync() { //init all sync stuff
 	//     }
 	// }
 
-    while (startThread_sem==SEM_FAILED) {
-	    if (errno == EEXIST) {
-	      printf("semaphore startSem already exists, unlinking and reopening\n");
-	      fflush(stdout);
-	      sem_unlink("startSem");
-	      startThread_sem = sem_open("startSem", O_CREAT|O_EXCL, 0466, 0);
-	    }
-	    else {
-	      printf("semaphore could not be opened, error # %d\n", errno);
-	      fflush(stdout);
-	      exit(1);
-	    }
-	}
+ //    while (startThread_sem==SEM_FAILED) {
+	//     if (errno == EEXIST) {
+	//       printf("semaphore startSem already exists, unlinking and reopening\n");
+	//       fflush(stdout);
+	//       sem_unlink("startSem");
+	//       startThread_sem = sem_open("startSem", O_CREAT|O_EXCL, 0466, 0);
+	//     }
+	//     else {
+	//       printf("semaphore could not be opened, error # %d\n", errno);
+	//       fflush(stdout);
+	//       exit(1);
+	//     }
+	// }
 
-    while (waitThread_sem==SEM_FAILED) {
-	    if (errno == EEXIST) {
-	      printf("semaphore waitSem already exists, unlinking and reopening\n");
-	      fflush(stdout);
-	      sem_unlink("waitSem");
-	      waitThread_sem = sem_open("waitSem", O_CREAT|O_EXCL, 0466, 0);
-	    }
-	    else {
-	      printf("semaphore could not be opened, error # %d\n", errno);
-	      fflush(stdout);
-	      exit(1);
-	    }
-	}
+ //    while (waitThread_sem==SEM_FAILED) {
+	//     if (errno == EEXIST) {
+	//       printf("semaphore waitSem already exists, unlinking and reopening\n");
+	//       fflush(stdout);
+	//       sem_unlink("waitSem");
+	//       waitThread_sem = sem_open("waitSem", O_CREAT|O_EXCL, 0466, 0);
+	//     }
+	//     else {
+	//       printf("semaphore could not be opened, error # %d\n", errno);
+	//       fflush(stdout);
+	//       exit(1);
+	//     }
+	// }
 }
 
 void closeSync() { //destroy all syncs
 	pthread_mutex_destroy(&avail_lock);
 	pthread_cond_destroy(&CSwait);
 	pthread_cond_destroy(&Mathwait);
-	pthread_cond_destroy(&CSgo);
-	pthread_cond_destroy(&Mathgo);	
-	// pthread_cond_Destroy(&cWaitAt1, NULL);
-	// pthread_cond_Destroy(&cWaitToBow0, NULL);
-	// pthread_cond_Destroy(&stopRowingback, NULL);
+	// pthread_cond_destroy(&CSgo);
+	// pthread_cond_destroy(&Mathgo);	
 
   // 	sem_close(CS_Here);
  	// sem_unlink("CSHere");
@@ -358,11 +303,11 @@ void closeSync() { //destroy all syncs
  	// sem_close(Math_Here);
  	// sem_unlink("MathHere");
 
- 	sem_close(waitThread_sem);
- 	sem_unlink("waitSem");
+//  	sem_close(waitThread_sem);
+//  	sem_unlink("waitSem");
 
- 	sem_close(startThread_sem);
- 	sem_unlink("startSem");
+//  	sem_close(startThread_sem);
+//  	sem_unlink("startSem");
 }
 
 // function to randomize list of integers
@@ -374,22 +319,4 @@ void shuffle(int* intArray, int arrayLen) {
     intArray[i] = intArray[r];
     intArray[r] = temp;
   }
-}
-
-/*
- * NOP function to simply use up CPU time
- * arg limit is number of times to run each loop, so runs limit^2 total loops
- */
-void delay( int limit )
-{
-  int j, k;
-
-  for( j=1; j < limit*1000; j++ )
-    {
-      for( k=1; k < limit*1000; k++ )
-        {
-            int x = j*k/(k+j);
-            int y = x/j + k*x - (j+5)/k + (x*j*k);
-        }
-    }
 }
